6744137a912d167afb46f428b7106f703c18b3c0
Junjun Zhang <junjun.ca@gmail.com>
2017-05-05 00:41:08 -0400

    first version

--- tools/upload.py
+++ tools/upload.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import time
+from random import randint
 from utils import get_task_dict, save_output_json
 
 task_dict = get_task_dict(sys.argv[1])
@@ -10,7 +11,7 @@ task_dict = get_task_dict(sys.argv[1])
 task_start = int(time.time())
 
 # do the real work here
-
+time.sleep(randint(1,10))
 
 
 # complete the task
@@ -18,8 +19,10 @@ task_start = int(time.time())
 task_stop = int(time.time())
 
 output_json = {
-    'task_start': task_start,
-    'task_stop': task_stop
+    'runtime': {
+        'task_start': task_start,
+        'task_stop': task_stop
+    }
 }
 
 save_output_json(output_json)
