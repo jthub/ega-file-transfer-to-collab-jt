585125f479dcc5f060b33bc2f0005240df1fda6f
Brice Aminou <brice.aminou@gmail.com>
2017-05-09 12:00:53 -0400

    Prepare metadata task added

--- tools/prepare_metadata_xml.py
+++ tools/prepare_metadata_xml.py
@@ -5,7 +5,7 @@ import sys
 import json
 import time
 from random import randint
-from utils import get_task_dict, save_output_json
+from utils import get_task_dict, save_output_json, get_md5
 
 task_dict = get_task_dict(sys.argv[1])
 cwd = os.getcwd()
@@ -40,7 +40,23 @@ ega_metadata_file_name = task_dict.get('input').get('ega_metadata_file_name')
 task_start = int(time.time())
 
 
-time.sleep(randint(1,10))
+#project_code = task_dict.get('input').get('project_code')
+#dataset = task_dict.get('input').get('ega_dataset_id')
+#experiment = input_json['input']['experiment_accession']
+#run = input_json['input']['run_accession']
+#sample = input_json['input']['sample_accession']
+#study = input_json['input']['study_accession']
+output_file = task_dict.get('input').get('ega_metadata_file_name')
+metadata_repo = task_dict.get('input').get('metadata_repo_url')
+
+subprocess.call(['prepare_ega_xml_audit.py',
+  '-i',metadata_repo,
+  '-p',project_code,
+  '-o',output_file,
+  '-d',ega_dataset_id,
+  '-e',experiment,
+  '-r',run,
+  '-sa',ega_sample_id])
 
 
 # complete the task
@@ -62,10 +78,10 @@ task_stop = int(time.time())
 """
 
 output_json = {
-    'xml_file': os.path.join(cwd, ega_metadata_file_name),
-    'xml_file_name': ega_metadata_file_name,
-    'xml_file_size': 23233,
-    'xml_file_md5sum': 'xxxxxxxx',
+    'xml_file': os.path.join(cwd, output_file),
+    'xml_file_name': output_file,
+    'xml_file_size': os.path.getsize(output_file),
+    'xml_file_md5sum': get_md5(output_file),
     'runtime': {
         'task_start': task_start,
         'task_stop': task_stop
