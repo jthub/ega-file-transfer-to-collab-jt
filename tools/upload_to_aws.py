018f56a3800d8f0317773c3eb9c70caa56d18199
Chen2x <chenchen199727@gmail.com>
2017-06-14 16:17:55 -0400

    added upload to aws

--- tools/upload_to_aws.py
+++ tools/upload_to_aws.py
@@ -28,11 +28,20 @@ file_ = task_dict.get('input').get('file')
 file_name = task_dict.get('input').get('file_name')
 file_md5sum = task_dict.get('input').get('file_md5sum')
 object_id = task_dict.get('input').get('object_id')
+idx_file_ = task_dict.get('input').get('idx_file')
+idx_file_name = task_dict.get('input').get('idx_file_name')
+idx_object_id = task_dict.get('input').get('idx_object_id')
+idx_file_md5sum = task_dict.get('input').get('idx_file_md5sum')
 
 if file_md5sum is None:
     file_md5sum = str(get_md5(file_))
 
+
+if idx_file_ and idx_file_md5sum is None:
+    idx_file_md5sum = str(get_md5(idx_file_))
+
 file_size = int(os.path.getsize(file_))
+
 
 task_start = int(time.time())
 
@@ -40,17 +49,15 @@ try:
     print subprocess.check_output(['icgc-storage-client','upload','--file', file_, '--object-id', object_id, '--md5', file_md5sum, '--force'])
 except Exception, e:
     with open('jt.log', 'w') as f: f.write(str(e))
+    sys.exit(1)
 
-    sys.exit(1)  # task failed
-
-# try:
-#     r = subprocess.check_output(['curl','https://raw.githubusercontent.com/jt-hub/ega-collab-transfer-tools/master/download_ega_file.py','|','python','-','-p',project_code,'-f', ega_file_id+".aes", '-o', file_name])
-# except Exception, e:
-#     print e
-#     sys.exit(1)  # task failed
-
-
-# complete the task
+if idx_object_id:
+    file_size+= + int(os.path.getsize(idx_file_))
+    try:
+        print subprocess.check_output(['icgc-storage-client','upload','--file', idx_file_, '--object-id', idx_object_id, '--md5', idx_file_md5sum, '--force'])
+    except Exception, e:
+        with open('jt.log', 'w') as f: f.write(str(e))
+        sys.exit(1)  # task failed
 
 task_stop = int(time.time())
 
@@ -58,6 +65,8 @@ task_stop = int(time.time())
 output_json = {
     'file': file_,
     'file_md5sum': file_md5sum,
+    'idx_file': idx_file_,
+    'idx_file_md5sum': idx_file_md5sum,
     'file_size' : file_size,
     'runtime': {
         'task_start': task_start,
