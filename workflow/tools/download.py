414181a239efbaba74fe9816aa3b8736f005aed8
Brice Aminou <brice.aminou@gmail.com>
2018-02-22 09:24:07 -0500

    Download step download multiple files instead of only one file

--- workflow/tools/download.py
+++ workflow/tools/download.py
@@ -24,23 +24,21 @@ cwd = os.getcwd()
       object_id:
         type: string
 """
-ega_file_id = task_dict.get('input').get('ega_file_id')
-file_name = task_dict.get('input').get('file_name')
-file_md5sum = task_dict.get('input').get('file_md5sum')
-object_id = task_dict.get('input').get('object_id')
+files = task_dict.get('input').get('files')
 project_code = task_dict.get('input').get('project_code')
 
 
 task_start = int(time.time())
 
-try:
-    if project_code in ['LINC-JP', 'BTCA-JP']:
-        r = subprocess.check_output(['download_ega_file.py','-p',project_code,'-f', str(ega_file_id)[-2:]+"/"+ega_file_id+".aes", '-o', file_name])
-    else:
-        r = subprocess.check_output(['download_ega_file.py','-p',project_code,'-f', ega_file_id+".aes", '-o', file_name])
-except Exception, e:
-    with open('jt.log', 'w') as f: f.write(str(e))
-    sys.exit(1)  # task failed
+for _file in files:
+    try:
+        if project_code in ['LINC-JP', 'BTCA-JP']:
+            r = subprocess.check_output(['download_ega_file.py','-p',project_code,'-f', str(_file.get('ega_file_id'))[-2:]+"/"+_file.get('ega_file_id')+".aes", '-o', _file.get('file_name')])
+        else:
+            r = subprocess.check_output(['download_ega_file.py','-p',project_code,'-f', _file.get('ega_file_id')+".aes", '-o', _file.get('file_name')])
+    except Exception, e:
+        with open('jt.log', 'w') as f: f.write(str(e))
+        sys.exit(1)  # task failed
 
 
 # complete the task
@@ -63,11 +61,7 @@ task_stop = int(time.time())
 """
 
 output_json = {
-    'file': os.path.join(cwd, file_name),
-    'ega_file_id': ega_file_id,
-    'file_name': file_name,  # we may need to deal with encrypted / unencypted file names
-    'object_id': object_id,
-    'file_md5sum': file_md5sum,
+    'out_dir': cwd,
     'runtime': {
         'task_start': task_start,
         'task_stop': task_stop
