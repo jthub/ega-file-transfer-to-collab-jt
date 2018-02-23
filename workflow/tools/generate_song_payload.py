e5f952b6e24ac8b883f06ff5df6ba5c4668614f8
Brice Aminou <brice.aminou@gmail.com>
2018-02-23 12:05:29 -0500

    fix of generate song payload

--- workflow/tools/generate_song_payload.py
+++ workflow/tools/generate_song_payload.py
@@ -17,8 +17,8 @@ task_dict = get_task_dict(sys.argv[1])
 cwd = os.getcwd()
 
 input_dir = task_dict.get('input').get('input_dir')
-files = task_dict.get('input').get('normal_bam')
-metadata_file_name = task_dict.get('input').get('tumor_bams')
+files = task_dict.get('input').get('files')
+metadata_file_name = task_dict.get('input').get('metadata_file_name')
 
 analysis_id = task_dict.get('input').get('analysis_id')
 analysis_type = task_dict.get('input').get('analysis_type')
@@ -57,14 +57,15 @@ def is_aligned(files):
 
 
 output_file = os.path.join(input_dir,'payload.json')
-experiment_payload = ExperimentPayload(aligned=is_aligned, library_strategy=library_strategy, reference_genome=reference_genome)
-song_payload = SongPayload(analysis_id=analysis_id, analysis_type=analysis_type, experiment_payload=experiment_payload)
+experiment_payload = ExperimentPayload(aligned=is_aligned(files), library_strategy=library_strategy, reference_genome=reference_genome)
+song_payload = SongPayload(analysis_id=analysis_id, analysis_type='sequencingRead', experiment_payload=experiment_payload)
 donor_payload = DonorPayload(donor_gender=donor_gender, donor_submitter_id=donor_submitter_id)
 specimen_payload = SpecimenPayload(specimen_class=get_specimen_class(specimen_type),
                                    specimen_type=specimen_type,specimen_submitter_id=specimen_submitter_id)
 sample_payload = SamplePayload(donor_payload=donor_payload, sample_submitter_id=sample_submitter_id, sample_type=sample_type, specimen_payload=specimen_payload)
+
+song_payload.add_sample_payload(sample_payload)
 song_payload.add_info('isPcawg', False)
-song_payload.to_json_file(output_file)
 
 for file in files:
     file_path = os.path.join(input_dir, file.get('file_name'))
@@ -80,6 +81,9 @@ song_payload.add_file_payload(FilePayload(file_access='controlled',
                           md5sum=hashlib.md5(open(file_path, 'rb').read()).hexdigest(),
                           file_type=get_file_type(metadata_file_name),
                           file_size=os.stat(file_path).st_size))
+
+
+song_payload.to_json_file(output_file)
 
 save_output_json({
     'payload': output_file
